c3c0c08f1bac6c1a59ce567428f69be9ff3d9a39
IrisDonVolt <irisodonvolt@gmail.com>
2026-04-22 10:39:36 +0530

    user and journal models created

--- ClearPlanApp/models.py
+++ ClearPlanApp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Users(models.Model):
+    username = models.CharField(max_length=None) # string 
+    theme = models.CharField(max_length=None) # string 
+    font = models.CharField(max_length=None)
+    taskbar_pref = models.CharField(max_length=None)
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
+    
+class Journal(models.Model):
+    title = models.CharField(max_length=50)
+    userid = models.ForeignKey(Users, on_delete=models.CASCADE)
+    bookcolor = models.CharField(max_length=None) 
+    bindercolor = models.CharField(max_length=None) 
+    coverimage = models.ImageField()
+    created_at = models.DateTimeField(default=datetime.now, blank=True) 
