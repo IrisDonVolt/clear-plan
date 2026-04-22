6e6c36edef702a80d54d08a13b681352b0738475
IrisDonVolt <irisodonvolt@gmail.com>
2026-04-22 11:21:51 +0530

    working on creating a journal in database

--- ClearPlanApp/urls.py
+++ ClearPlanApp/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.firstpage, name='firstpage'),
     path('register', views.register, name='register'),
-    path('home', views.home, name='home'), 
+    path('home/<str:mode>', views.home, name='home'), 
     path('editjournal', views.editjournal, name='editjournal')
 ]
--- ClearPlanApp/views.py
+++ ClearPlanApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages 
+from .models import Users, Journal
 
 # Create your views here.
 def firstpage(request):
@@ -23,14 +24,39 @@ def register(request):
         else: 
             user = User.objects.create_user(username=username, email=email, password=password)
             user.save()
+            
+            # create user object in Users database 
+            user_object = Users(
+                username=username, 
+                theme="default",
+                font="default",
+                taskbar_pref="default" 
+            )
+            user_object.save()       
             return redirect('login')
             
             
     else: 
         return render(request, 'register.html')
 
-def home(request): 
-    return render(request, 'home.html')
+def home(request, mode): 
+    current_user = request.user 
+    if current_user.is_authenticated: 
+        current_user_id = current_user.id 
+        current_user_username = current_user.username 
+        
+        context = {
+            'id': current_user_id, 
+            'username': current_user_username
+        }
+        
+        journal = Journal.objects.filter(userid=current_user_id)
+        if journal: 
+            mode = f"{current_user_username}"
+        else: 
+            mode = "new"
+        
+        return render(request, 'home.html', {'context': context, 'mode': mode})
 
 def editjournal(request): 
     return render(request, 'editjournal.html')
